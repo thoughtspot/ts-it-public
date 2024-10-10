cac7e03decb14c242199d4504950c93ef417cbda
Pravin <2pravin7@users.noreply.github.com>
2024-10-10 09:51:37 -0700

    Update git_sync.sh

--- git_sync.sh
+++ git_sync.sh
@@ -3,6 +3,6 @@
 
 # Git operations
 git add .
-git commit -m "ASN Based IPs updated on - $(date +%F-%T)"  # Commit with timestamp
+git commit -m "ASN Based IPs updated on - $(date +%F-%H:%M)"  # Commit with timestamp
 git pull --rebase  # Pull latest changes and rebase
 git push  # Push local commits
