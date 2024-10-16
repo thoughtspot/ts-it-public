9fc07269e5de0457e5df4252dd17727e0fec9216
ts-it-security <ts-it-security>
2024-10-16 10:44:09 -0700

    ASN Based IPs updated on - 2024-10-16-10:44

--- git_sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# Git operations
-git add .
-git commit -m "ASN Based IPs updated on - $(date +%F-%H:%M)"  # Commit with timestamp
-git pull --rebase  # Pull latest changes and rebase
-git push  # Push local commits
